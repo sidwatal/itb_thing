6f85b23cdb7c3e573f5fc43e96e674a066b8b4d2
Sid <sid.watal@gmail.com>
2024-02-28 22:53:03 -0500

    add first set of pilot specs

--- spec/models/pilot_spec.rb
+++ spec/models/pilot_spec.rb
@@ -1,5 +1,20 @@
+# spec/models/pilot_spec.rb
 require 'rails_helper'
 
 RSpec.describe Pilot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'fields' do
+    it { is_expected.to have_db_column(:name).of_type(:string) }
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to have_db_column(:nickname).of_type(:string) }
+    it { is_expected.to respond_to(:nickname) }
+    it { is_expected.to have_db_column(:debug_name).of_type(:string) }
+    it { is_expected.to respond_to(:debug_name) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:debug_name) }
+    it { is_expected.to validate_uniqueness_of(:debug_name) }
+  end
 end
