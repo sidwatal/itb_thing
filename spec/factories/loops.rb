1bc745c5606d939d95065a9a59b6f732f232eb84
Sid <sid.watal@gmail.com>
2024-03-04 22:44:40 -0500

    add run loop association

--- spec/factories/loops.rb
+++ spec/factories/loops.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :loop do
     profile_name { Faker::TvShows::Simpsons.character }
-    # starting_pilot factory: :pilot
+    starting_pilot factory: :pilot
   end
 end
